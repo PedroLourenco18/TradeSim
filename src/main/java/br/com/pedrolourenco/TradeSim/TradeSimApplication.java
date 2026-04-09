b307a78e6fa3dff691365e82545086501fd49135
Pedro Eduardo Lourenço <pedroedu2007@gmail.com>
2026-04-09 19:57:04 -0300

    feat: add basic h2 and jpa config

---
+++ src/main/java/br/com/pedrolourenco/TradeSim/TradeSimApplication.java
@@ -0,0 +1,13 @@
+package br.com.pedrolourenco.TradeSim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradeSimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradeSimApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/pedrolourenco/TradeSim/TradeSimApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.pedrolourenco.TradeSim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradeSimApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
