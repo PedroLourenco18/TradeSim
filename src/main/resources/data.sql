0cfb7c12dcf0508f66f3b617efd8c1f2e5d17352
Pedro Eduardo Lourenço <pedroedu2007@gmail.com>
2026-05-18 18:36:42 -0300

    feat: create stocks insertion SQL file

---
+++ src/main/resources/data.sql
@@ -0,0 +1,11 @@
+INSERT INTO stocks (id, ticker, name, active) VALUES
+                                                  (RANDOM_UUID(), 'PETR4', 'Petrobras PN', true),
+                                                  (RANDOM_UUID(), 'VALE3', 'Vale ON', true),
+                                                  (RANDOM_UUID(), 'ITUB4', 'Itaú Unibanco PN', true),
+                                                  (RANDOM_UUID(), 'BBDC4', 'Bradesco PN', true),
+                                                  (RANDOM_UUID(), 'BBAS3', 'Banco do Brasil ON', true),
+                                                  (RANDOM_UUID(), 'WEGE3', 'WEG ON', true),
+                                                  (RANDOM_UUID(), 'ABEV3', 'Ambev ON', true),
+                                                  (RANDOM_UUID(), 'MGLU3', 'Magazine Luiza ON', true),
+                                                  (RANDOM_UUID(), 'RENT3', 'Localiza ON', true),
+                                                  (RANDOM_UUID(), 'SUZB3', 'Suzano ON', true);
